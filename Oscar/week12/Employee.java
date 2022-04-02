0bc3ed13b39d10428f759b97449d080ceac428fd
Burak Arıkboğa <barikboga42@gmail.com>
2022-04-02 23:38:01 +0300

    Value.java modified

--- Oscar/week12/Employee.java
+++ Oscar/week12/Employee.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Employee {
     private static int ID = 1;
     String name, jobTitle;
-    double salary;
+    private double salary;
     int id;
 
     public Employee(String name, String jobTitle, double salary) {
@@ -19,6 +19,10 @@ public class Employee {
         this.id = ID++;
     }
 
+    public double getSalary() {
+        return salary;
+    }
+
     public static void main(String[] args) {
         Employee e1 = new Employee("yasin", "SDET", 150000);
         Employee e2 = new Employee("erol", "QA Lead", 170000);
@@ -28,8 +32,10 @@ public class Employee {
 
         Employee[] testers = {e1, e2, e3, e4, e5};
 
-        double maxSalary = Arrays.stream(testers).mapToDouble(p -> p.salary).max().orElse(-1);
+        double maxSalary = Arrays.stream(testers).mapToDouble(Employee::getSalary).max().orElse(-1);
         System.out.println(maxSalary);
+
+        System.gc();
 
         for (Employee e : testers) {
             System.out.println(e.id);
