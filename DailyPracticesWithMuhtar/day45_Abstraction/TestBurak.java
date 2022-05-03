273d5d584df353f8cfa5d81c6cbebe92931d8c2c
Burak Arıkboğa <barikboga42@gmail.com>
2022-05-03 19:53:05 +0300

    Read comment lines inside the TestBurak.java filee.
    Some trick points have been emphasized.

--- DailyPracticesWithMuhtar/day45_Abstraction/TestBurak.java
+++ DailyPracticesWithMuhtar/day45_Abstraction/TestBurak.java
@@ -1,5 +1,16 @@
 package day45_Abstraction;
 
+abstract class Man {
+    public abstract String lastName();
+
+    public int defaultMethod() {
+        return 1;
+    }
+
+    public static int staticMethod() {
+        return 11;
+    }
+}
 
 interface Parent {
     float PI = 3.14F;
@@ -26,26 +37,40 @@ interface OtherParent {
 }
 
 interface Child extends Parent, OtherParent {
-
-
 }
 
 /**
  * There cannot be a default method or instance variable with the same name
  * in implemented interfaces at the same time.
  */
-public class TestBurak implements Child {
+public class TestBurak extends Man implements Child {
+    @Override
+    public String lastName() {
+        return "Arıkboğa";
+    }
 
     public static void main(String[] args) {
 
-        // Interface's default methods and instance variables can be accessed from any child class and interface
+        /*
+         - Interface's default methods and instance variables can be accessed from any child class and interface
+         - If we have more than one method with same name which are inherited from parent class and parent interface,
+         class' methods override the interface method.
+        */
         System.out.println(new TestBurak().defaultMethod());
+
+        /*
+        Static methods can be inherited only with using "extend" keyword.
+        and we can access it with using child class name
+        */
+        System.out.println(TestBurak.staticMethod());
         System.out.println(TestBurak.PI);
 
-        // Interface's static methods can only be accessed via own Interface name
-        // Child.staticMethod(); // this code line gives error as "Static method may be invoked on containing interface only"
+        /*
+         - Interface's static methods can only be accessed via own Interface name
+         they can not be accessed with using subclass name.
+         - Child.staticMethod(); // this code line gives error as "Static method may be invoked on containing interface only"
+        */
         System.out.println(Parent.staticMethod());
-
 
         // Interface's default methods and instance variables can be accessed from any child class and interface
         System.out.println(new TestBurak().otherDefaultMethod());
@@ -54,6 +79,5 @@ public class TestBurak implements Child {
         // Interface's static methods can only be accessed via own Interface name
         // Child.staticMethod(); // this code line gives error as "Static method may be invoked on containing interface only"
         System.out.println(OtherParent.staticMethod());
-
     }
 }
