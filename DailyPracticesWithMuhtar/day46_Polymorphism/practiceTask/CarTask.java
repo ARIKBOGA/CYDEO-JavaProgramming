a92db100007f674d7963316bf3632d5edc642d8e
Burak Arıkboğa <barikboga42@gmail.com>
2022-05-05 13:59:00 +0300

    Day_46 practice tasks have been solved

---
+++ DailyPracticesWithMuhtar/day46_Polymorphism/practiceTask/CarTask.java
@@ -0,0 +1,52 @@
+package day46_Polymorphism.practiceTask;
+
+
+import day38_Inheritance.carTask.BMW;
+import day38_Inheritance.carTask.Car;
+import day38_Inheritance.carTask.Tesla;
+import day38_Inheritance.carTask.Toyota;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarTask {
+    public static void main(String[] args) {
+        Car[] cars = {
+                new Toyota("Highlander", 2010, 25000, "White", 255000),
+                new BMW("X5", 2014, 32000, "Red", 12000),
+                new Toyota("Corolla", 2011, 20000, "White", 310000),
+                new BMW("M3", 2015, 33000, "Blue", 14030),
+                new BMW("M5", 2017, 35000, "Black", 15000),
+                new BMW("M4", 2018, 40000, "White", 19000),
+                new BMW("7 Series", 2009, 30000, "Purple", 21000),
+                new BMW("i3", 2011, 30000, "Black", 9000),
+                new Toyota("Camry", 2018, 30000, "Red", 185000),
+                new Toyota("Rav4", 2012, 23000, "Red", 285000),
+                new Toyota("Avalon", 2019, 35000, "Blue", 175230),
+                new Tesla("Model 3", 2015, 45000, "White", 235000),
+                new Tesla("Model Y", 2017, 65000, "Black", 135000),
+                new Tesla("Model X", 2016, 48000, "White", 235000),
+                new Tesla("Model X", 2014, 48000, "White", 236000),
+        };
+
+        List<Car> eligibleForRecall = Arrays.stream(cars)
+                .filter(p -> (p.brand.equalsIgnoreCase("toyota") && p.year <= 2011 && p.year >= 2010)
+                        || (p.brand.equalsIgnoreCase("bmw") && p.year <= 2002 && p.year >= 1929)
+                        || (p.brand.equalsIgnoreCase("tesla") && p.year <= 2016 && p.year >= 2015))
+                .sorted(Comparator.comparing(p -> p.year))
+                .collect(Collectors.toList());
+
+        eligibleForRecall.forEach(System.out::println);
+
+        List<Car> sorted = Arrays.stream(cars)
+                .sorted(Comparator.comparing(p -> p.miles))
+                .collect(Collectors.toList());
+        Car highestMileage = sorted.get(0);
+        Car lowestMileage = sorted.get(sorted.size() - 1);
+
+        System.out.println("highestMileage = " + highestMileage);
+        System.out.println("lowestMileage = " + lowestMileage);
+    }
+}
---
+++ DailyPracticesWithMuhtar/day46_Polymorphism/practiceTask/EmployeeTask.java
@@ -0,0 +1,57 @@
+package day46_Polymorphism.practiceTask;
+
+import day43_Abstraction.employeeTask.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmployeeTask {
+    public static void main(String[] args) {
+        Employee[] employees = {
+                new Tester("Sherali", 32, 'M', 1, "QA", 110000),
+                new Developer("Zuhal", 27, 'F', 2, "Java Developer", 128000),
+                new Tester("George", 28, 'M', 3, "SDET", 120000),
+                new Developer("Cristina", 23, 'F', 4, "Software Developer", 135000),
+                new Tester("Cihad", 30, 'M', 5, "SDET", 105000),
+                new Developer("Gulistan", 26, 'F', 6, "Web Developer", 130000),
+                new Tester("Yuliya", 28, 'F', 7, "QE", 120000),
+                new Developer("Cassendra", 29, 'F', 8, "Front-end Developer", 140000),
+                new Tester("Aygu", 25, 'F', 9, "SDET", 130000),
+                new Developer("Sophie", 26, 'F', 10, "Back-end Developer", 150000),
+                new Tester("Timur", 29, 'M', 11, "SDET", 115000),
+                new Developer("Fady", 26, 'F', 12, "Full Stack Developer", 142000),
+                new Tester("Iryna", 24, 'F', 13, "QE", 125000),
+                new Developer("Enes", 26, 'M', 14, "Full Stack Developer", 142000),
+                new Tester("Mikael", 30, 'M', 15, "SDET", 105000),
+        };
+
+
+        List<Employee> testers = Arrays.stream(employees)
+                .filter(p -> p.getClass().getSimpleName().equalsIgnoreCase("tester"))
+                .collect(Collectors.toList());
+
+        List<Employee> developers = Arrays.stream(employees)
+                .filter(p -> p.getClass().getSimpleName().equalsIgnoreCase("developer"))
+                .collect(Collectors.toList());
+
+        List<Employee> scrumMembers = new ArrayList<>();
+        scrumMembers.addAll(developers);
+        scrumMembers.addAll(testers);
+
+        Tester maxSalaryTester = (Tester) testers.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                .collect(Collectors.toList())
+                .get(0);
+
+        Developer maxSalaryDeveloper = (Developer) developers.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                .collect(Collectors.toList())
+                .get(0);
+
+        System.out.println("maxSalaryDeveloper = " + maxSalaryDeveloper.getName() + ": " + maxSalaryDeveloper.getSalary());
+        System.out.println("maxSalaryTester = " + maxSalaryTester.getName() + ": " + maxSalaryTester.getSalary());
+    }
+}
