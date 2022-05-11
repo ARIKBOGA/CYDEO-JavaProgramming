415e005a02086e0b54d0df727382f4a17a5b3035
burakarikboga <barikboga42@gmail.com>
2022-05-11 15:35:47 +0300

    Exception 1 project is modified
    The constructor calls super(String)

--- Replit/Projects/Exception_1/PanicException.java
+++ Replit/Projects/Exception_1/PanicException.java
@@ -1,10 +1,8 @@
 package Projects.Exception_1;
 
 public class PanicException extends RuntimeException{
-    private static final String message ="Don't Panic :D";
 
     public PanicException(){
-        super();
-        System.out.println(message);
+        super("Don't Panic :D");
     }
 }
